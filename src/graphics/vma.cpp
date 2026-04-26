541b4d668137ff4baae135dffd7fad0390c415e2
n1coc4cola <n1coc4cola@gmail.com>
2026-04-26 20:15:49 +0200

    VMA usage, use cpp print funcs instead of c ones

--- src/graphics/vma.cpp
+++ src/graphics/vma.cpp
@@ -6,14 +6,14 @@
 
 #ifdef VMA_USE_DEBUG_LOG
 
-#include <string>
 #include <cstring>
-
+#include <print>
+#include <string>
 
 static int allocationCounter = 0;
 
-template<typename ... Args>
-inline void vma_printer(const char *format, Args ... args)
+template<typename... Args>
+inline void vmaPrinter(const char *format, Args... args)
 {
 	std::string str("VMA LOG: ");
 	str += format;
@@ -21,35 +21,35 @@ inline void vma_printer(const char *format, Args ... args)
 	printf(str.c_str(), args...);
 
 	if (strstr(format, "Allocate")) {
-		printf("Alloc\n");
-		allocationCounter++;
-	} else if (strstr(format, "Free")) {
-		printf("Dealloc\n");
-		allocationCounter--;
-	}
+        std::println("Alloc");
+        allocationCounter++;
+    } else if (strstr(format, "Free")) {
+        std::println("Dealloc\n");
+        allocationCounter--;
+    }
 }
 
 template<>
-inline void vma_printer(const char *format)
+inline void vmaPrinter(const char *format)
 {
 	std::string str("VMA LOG: ");
 	str += format;
 	str += "\n";
-	printf("%s", str.c_str());
+    std::print("{}", str);
 
-	if (strstr(format, "Allocate")) {
-		printf("Alloc\n");
-		allocationCounter++;
-	} else if (strstr(format, "Free")) {
-		printf("Dealloc\n");
-		allocationCounter--;
-	}
+    if (strstr(format, "Allocate")) {
+        std::println("Alloc");
+        allocationCounter++;
+    } else if (strstr(format, "Free")) {
+        std::println("Dealloc");
+        allocationCounter--;
+    }
 }
 
 #define VMA_DEBUG_LOG(format, ...) \
-do { \
-	    vma_printer(format, ##__VA_ARGS__); \
-} while (false)
+    do { \
+        vmaPrinter(format, ##__VA_ARGS__); \
+    } while (false)
 
 #endif
 
