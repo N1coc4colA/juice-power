db0c11040ef34a4f8c5b0cd28da1bb904a54f180
n1coc4cola <n1coc4cola@gmail.com>
2026-09-21 10:48:33 +0200

    Make vkCheck output flag instead of enum value name

--- src/graphics/defines.cpp
+++ src/graphics/defines.cpp
@@ -1,15 +1,18 @@
 #include "src/graphics/defines.h"
 
+#include <cstdio>
+
 #include <fmt/ostream.h>
 
-#include <magic_enum.hpp>
+#include <magic_enum_flags.hpp>
 
 namespace Graphics {
 
 void vkCheck(const VkResult error)
 {
     if (error != VK_SUCCESS) {
-        fmt::print("Detected Vulkan error: {}", magic_enum::enum_name(error));
+        fmt::print("Detected Vulkan error: {} {}\n", static_cast<int64_t>(error), magic_enum::enum_flags_name(error));
+        std::fflush(nullptr);
         abort();
     }
 }
